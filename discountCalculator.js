e2ffae49994a7d2764f80560fa4b349488752a4f
robert01mm <mmasirobert08@gmail.com>
2025-12-02 23:10:02 +0300

    initial commit

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,22 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger;
+
+    for (let i = 0; i < quantity; i++) {
+        debugger;
         totalPrice += pricePerItem;
     }
 
+    debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
+    debugger;
     return totalPrice;
 }
 
-
-
 module.exports = calculateDiscountedPrice;
+
+// TEST RUN
+console.log(calculateDiscountedPrice(5, 100));
